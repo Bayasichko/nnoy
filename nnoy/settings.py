d70c94fce7fce2c732d2dc738d3aff8a4c3fce9c
Bayasichko <kadyraliev345@gmail.com>
2025-05-28 04:40:08 +0600

    Added STATIC_ROOT

--- nnoy/settings.py
+++ nnoy/settings.py
@@ -123,6 +123,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 
